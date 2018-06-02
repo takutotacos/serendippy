62fea91eb328a12ebcfe14ed8f53a3f6c4ffa7a9
takutotacos <s.takuto0214@gmail.com>
2018-06-02 16:27:19 +0900

    不要なフィールドを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180530050945) do
+ActiveRecord::Schema.define(version: 20180602072315) do
 
   create_table "discoveries", force: :cascade do |t|
     t.string "content"
