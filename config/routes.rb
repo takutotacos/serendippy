8eaa46a3c6be959c946d2e038c46f662f0e49fd8
takuto.sugita <takuto.sugita@jmty.jp>
2018-06-06 06:41:05 +0900

    define routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/login' => 'sessions#destroy'
+
   resources :users
   resources :discoveries
 
-  get "/top" => "users#index"
+  get '/top' => 'users#index'
 end
