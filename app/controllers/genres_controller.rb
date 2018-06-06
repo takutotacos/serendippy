5ef9694f04b6a851d8b07ebf93257b626d338b16
takuto.sugita <takuto.sugita@jmty.jp>
2018-06-06 18:47:09 +0900

    ジャンルの登録ができるように下準備

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,22 @@
+class GenresController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+private
+  def _genre_params
+  end
+end
