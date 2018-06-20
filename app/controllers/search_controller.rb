d5dbbb77c1e32818ab3c047dddeb5b0f51db5e0d
takuto.sugita <takuto.sugita@jmty.jp>
2018-06-20 09:19:42 +0900

    検索画面を一旦終了

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,37 +1,21 @@
 class SearchController < ApplicationController
   def new
+    @search = Search.new
+    @search.genres = Genre.all
     @discoveries = Discovery.where(user_id: current_user.id).order(created_at: :desc)
-    @genres = Genre.all
   end
 
   def show
-    of_genres = params[:search][:genre].present? ? params[:search][:genre][:id] : []
-    content = params[:search][:content]
-    before_time = params[:search][:time_span].present? ? params[:search][:time_span][:id] : []
-    before_time = if before_time.blank?
-                    ""
-                  elsif before_time == 0
-                    1.day.ago
-                  elsif before_time == 1
-                    7.day.ago
-                  elsif before_time == 2
-                    1.month.ago
-                  elsif before_time == 3
-                    3.month.ago
-                  end
-    @genres = Genre.all
-    @discoveries = Discovery.where(user_id: current_user.id)
+    @search = Search.new(_search_params)
+    @search.genres = Genre.all
+    @discoveries = @search.search(current_user)
 
-    # todo 検索が複数回走っちゃうので、一回で終わらせられるように処理を書き換える。
-    @discoveries = @discoveries.where("content like '%#{content}%'") if content.present?
-    @discoveries = @discoveries.joins(:discovery_and_genre_relationships).where(discovery_and_genre_relationships: { genre_id: of_genres }) if of_genres.present?
-    @discoveries = @discoveries.where(:created_at => before_time) if before_time.present?
     render :new
   end
 
 private
 
-  def search_params
-    params.require("search").permit(:content, :genre_ids)
+  def _search_params
+    params.require("search").permit(:content, :created_at, :genres => [])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180618125722) do
+ActiveRecord::Schema.define(version: 20180619230028) do
 
   create_table "discoveries", force: :cascade do |t|
     t.string "content"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "genres_id"
-    t.index ["genres_id"], name: "index_discoveries_on_genres_id"
     t.index ["user_id"], name: "index_discoveries_on_user_id"
   end
 
