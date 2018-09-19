446010cee71ff42b67668abaf9b3d0480d4ff849
takutotacos <s.takuto0214@gmail.com>
2018-09-19 06:12:57 -0500

    change migration file for heroku

--- db/migrate/20180608044441_change_relation_to_discovery.rb
+++ db/migrate/20180608044441_change_relation_to_discovery.rb
@@ -1,6 +1,5 @@
 class ChangeRelationToDiscovery < ActiveRecord::Migration[5.1]
   def change
-    remove_reference(:discovery_and_genre_relationships, :memo, index: true, foreign_key: true)
     add_reference(:discovery_and_genre_relationships, :discovery, index: true, foreign_key: true)
   end
 end
